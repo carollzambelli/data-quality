4a070e5af7fd3c672ecc6c5397e6f939147c2d4e
unknown <caroll.zambelli@gmail.com>
2024-11-13 22:40:34 -0300

    add report

--- src/validator.py
+++ src/validator.py
@@ -16,7 +16,6 @@ class Validator:
         self.i_limits = self.configs.loc[self.configs['limits_validation'] == 1].index
         self.keys = list(self.configs.loc[self.configs['key_validation'] == 1]["variavel"])
         self.i_null = self.configs.loc[self.configs['null_validation'] == 1].index
-        self.i_dist = self.configs.loc[self.configs['distribution_validation'] == 1].index
 
     def key_validation(self):
         if self.data[self.keys].nunique().item() != len(self.data):
@@ -30,15 +29,6 @@ class Validator:
             n_null = len(self.data.loc[self.data[col].isnull()])/len(self.data)
             if not value - value_dev <= n_null <= value + value_dev:
                 logger.warning(f"NULL_TOLERANCE ; {col}; {datetime.datetime.now()}")
-
-    def distribution_validation(self):
-        for i in self.i_dist:
-            value = self.configs.iloc[i]["value"]
-            col = self.configs.iloc[i]["variavel"]
-            categ = self.configs.iloc[i]["category"]
-            value_dev = self.configs.iloc[i]["value_dev"]
-            if not value - value_dev <= len(self.data[self.data[col] == categ]) <= value + value_dev:
-                logger.warning(f"DISTRIBUTION_DEVIATION ; {col}; {datetime.datetime.now()}")
                 
     def limits_validation(self):
         for i in self.i_limits:
--- tests/quality_test.py
+++ tests/quality_test.py
@@ -1,7 +1,21 @@
-import os
-from app import prepare
+import pandas as pd
+import json
+from sklearn.pipeline import Pipeline
+from src.prepare import Prepare
 
 def test_prepare_cols():
-    config_path = str(os.environ.get('CONFIG_PATH'))
-    df, configs = prepare(config_path)
-    assert set(df.columns) == set(configs["output_cols"])
+    output_cols = ["id", "preco", "quartos", "banheiros", "area_util", "area_total", "andares", "vista_mar", "qualidade_vista"]
+    configs = json.load(open("assets/path_config.json"))
+    meta_schema = pd.read_excel(configs["meta_path"], sheet_name="schema")
+    df = pd.read_csv(configs["data_path"])
+    pp = Prepare(df, meta_schema)
+
+    Pipeline([
+        ('seleciona_colunas', pp.select_cols()),
+        ('renomeia colunas', pp.rename_cols()),
+        ('atribui tipos', pp.data_type()),
+        ])
+
+    df = pp.return_data()
+
+    assert set(df.columns) == set(output_cols)
